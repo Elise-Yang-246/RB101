1fbaa0d3424adbed7a991de6c0502c38ed771739
Elise-Yang-246 <elise.codes.daily@gmail.com>
2023-10-19 22:32:27 +0000

    Pseudo-Code Practice Example 1

---
+++ lesson_2_small_programs/pseudocode_practice_ex_1.rb
@@ -0,0 +1,31 @@
+# Write out pseudo-code (both casual and formal) for:
+# a method that returns the sum of two integers
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# Casual pseudo-code:
+Get user input for 2 integers.
+Add 2 integers together.
+Return the added value.
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#Formal pseudo-code:
+START
+
+Define method called sum
+
+GET user input for 1st integer
+SET integer1 = 1st integer
+
+GET user input for 2nd integer
+SET integer2 = 2nd integer
+
+SET sum = integer1 + integer2
+PRINT sum
+
+END
