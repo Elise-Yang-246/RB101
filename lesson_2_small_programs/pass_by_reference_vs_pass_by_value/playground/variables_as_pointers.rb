f103dfc1d07c2199a0e9dedd73eac35da854c61f
Elise-Yang-246 <elise.codes.daily@gmail.com>
2023-11-10 17:53:53 +0000

    Pass by Reference vs Pass by Value -- Review of Variables as Pointers

---
+++ lesson_2_small_programs/pass_by_reference_vs_pass_by_value/playground/variables_as_pointers.rb
@@ -0,0 +1,50 @@
+a = [1, 2, 3, 3]
+b = a
+c = []
+
+puts "Original values:"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "----------------------"
+
+c = a.uniq!
+
+puts "After c = a.uniq!"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "----------------------"
+
+a << 4
+
+puts "After a << 4"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "----------------------"
+
+c << 4
+
+puts "After c << 4"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "----------------------"
+
+d = b.uniq
+puts "After d = b.uniq"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "d: #{d}"
+puts "----------------------"
+
+e = b.pop
+
+puts "After d = b.pop"
+puts "a: #{a}"
+puts "b: #{b}"
+puts "c: #{c}"
+puts "d: #{d}"
+puts "e: #{e}"
